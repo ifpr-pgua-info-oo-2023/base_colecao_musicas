3641d365d9ffe69d559cadb8f72a873194e33cf6
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2023-06-19 18:57:33 -0300

    código inicial

---
+++ src/main/java/ifpr/pgua/eic/listatelefonica/App.java
@@ -0,0 +1,36 @@
+package ifpr.pgua.eic.listatelefonica;
+
+import ifpr.pgua.eic.listatelefonica.controllers.Principal;
+import io.github.hugoperlin.navigatorfx.BaseAppNavigator;
+import io.github.hugoperlin.navigatorfx.ScreenRegistryFXML;
+
+/**
+ * JavaFX App
+ */
+public class App extends BaseAppNavigator {
+
+    
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public String getHome() {
+        // TODO Auto-generated method stub
+        return "PRINCIPAL";
+    }
+
+
+    @Override
+    public String getAppTitle() {
+        // TODO Auto-generated method stub
+        return "Lista Telefônica";
+    }
+
+    @Override
+    public void registrarTelas() {
+        registraTela("PRINCIPAL", new ScreenRegistryFXML(App.class, "principal.fxml", o->new Principal()));
+    }
+
+}
